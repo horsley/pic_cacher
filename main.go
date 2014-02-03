ae0c70b271572dbcdef60bc65c07865912925b4f
horsley <imhorsley@gmail.com>
2014-02-03 18:26:38 +0800

    lock problem fix

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"sync"
 	"time"
 )
 
 var APP_DIR, CACHE_DIR string
-var makingId map[string]chan bool
+var makingId map[string]*sync.Mutex
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -28,7 +29,7 @@ func main() {
 		log.Println("Cache dir:", CACHE_DIR)
 	}
 
-	makingId = make(map[string]chan bool)
+	makingId = make(map[string]*sync.Mutex)
 
 	http.HandleFunc("/pic", getPic)
 	http.HandleFunc("/job", getPicJob)
@@ -49,43 +50,25 @@ func getPic(w http.ResponseWriter, req *http.Request) {
 	picId := getCacheId(picUrl)
 	log.Println("requesting pic id:", picId)
 
-	if lock, ok := makingId[picId]; ok { //证明正在做这个id的cache
-		log.Println("another one doing cache for pic id:", picId)
-		<-lock //完成了
-		close(lock)
-		delete(makingId, picId)
-	}
-
 	if !cacheExist(picId) {
 		log.Println("cache miss, id:", picId)
-		go func() {
-			for { //等待makeCache完成
-				if lock, ok := makingId[picId]; ok {
-					<-lock //完成了
-					close(lock)
-					delete(makingId, picId)
-					break
-				}
-				runtime.Gosched()
-			}
-		}()
-		cacheContent, err = makeCache(picUrl)
-		if err != nil {
+
+		if err := makeCache(picUrl); err != nil {
 			log.Println("make cache error, id:", picId, "error:", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 	} else {
 		log.Println("cache hit, pic id:", picId)
-		//读取缓存
-		cacheContent, err = cacheRead(picId)
-		if err != nil {
-			log.Println("cache read error, id:", picId, "error:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
 	}
 
+	//读取缓存
+	cacheContent, err = cacheRead(picId)
+	if err != nil {
+		log.Println("cache read error, id:", picId, "error:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.Write(*cacheContent)
 	log.Println("serve pic done, id:", picId)
 }
@@ -105,42 +88,44 @@ func getPicJob(w http.ResponseWriter, req *http.Request) {
 }
 
 //制作缓存
-func makeCache(url string) (data *[]byte, err error) {
+func makeCache(url string) (err error) {
 	var respBody []byte
 	picId := getCacheId(url)
-	makingId[picId] = make(chan bool)
-	defer func() {
-		makingId[picId] <- false
-		log.Println("makeCache done, id:", picId)
-	}()
+
+	makingId[picId].Lock()
+	defer makingId[picId].Unlock()
+	if cacheExist(picId) {
+		//可能别的goroutine也在做这个id的cache,拿到锁正是别人做完的时候
+		log.Println("make cache job done by other goroutine")
+		return nil
+	}
 
 	log.Println("making cache, url:", url)
-
 	//请求远端
 	for i := 0; i < 5; i++ { //最大重试次数
 		resp, err := http.Get(url)
 		if err != nil {
 			log.Println("request remote fail, exiting, url:", url)
-			return nil, err
+			return err
 		}
 		if resp.StatusCode != 200 {
 			log.Println("request remote fail, code:", resp.StatusCode, "waiting for retry")
-			time.Sleep((i + 1) * 500 * time.Millisecond)
+			time.Sleep(time.Duration((i+1)*500) * time.Millisecond)
 		} else {
 			respBody, _ = ioutil.ReadAll(resp.Body)
-			resp.Body.Close()
+			defer resp.Body.Close()
 			log.Println("request remote done, size:", len(respBody), "id:", picId)
+			break
 		}
-
 	}
 
 	//存入缓存
 	err = cacheWrite(picId, &respBody)
 	if err != nil {
 		log.Println("cache save fail, id:", picId)
-		return nil, err
+		return err
 	} else {
 		log.Println("cache save succeed, id:", picId)
-		return &respBody, nil
+		return nil
 	}
 }
